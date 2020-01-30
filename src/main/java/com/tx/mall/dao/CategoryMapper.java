2e3f4bccb5ef0573ba2813329b8c90ac68d3dc3c
邓国兴 <denggx@126.com>
2020-01-30 17:41:17 +0800

    代码生成器mvn mybatis-generator:generate

--- src/main/java/com/tx/mall/dao/CategoryMapper.java
+++ src/main/java/com/tx/mall/dao/CategoryMapper.java
@@ -1,16 +1,17 @@
 package com.tx.mall.dao;
 
 import com.tx.mall.pojo.Category;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-@Mapper
 public interface CategoryMapper {
+    int deleteByPrimaryKey(Integer id);
 
-    @Select("select * from mall_category where id = #{id}")
-    Category findById(@Param("id") Integer id);
+    int insert(Category record);
 
+    int insertSelective(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
 }
