09cfe02ccd42cb2d05004e6a78ba3438d1d4e752
邓国兴 <denggx@126.com>
2020-02-02 09:10:41 +0800

    第8章测试完

--- src/test/java/com/tx/mall/service/impl/UserServiceImplTest.java
+++ src/test/java/com/tx/mall/service/impl/UserServiceImplTest.java
@@ -1,10 +1,13 @@
 package com.tx.mall.service.impl;
 
 import com.tx.mall.MallApplicationTests;
+import com.tx.mall.enums.ResponseEnum;
 import com.tx.mall.enums.RoleEnum;
 import com.tx.mall.pojo.User;
 import com.tx.mall.service.IUserService;
-
+import com.tx.mall.vo.ResponseVo;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,11 +20,17 @@ public class UserServiceImplTest extends MallApplicationTests {
 
     public static final String PASSWORD = "123456";
 
-    @Test
+    @Before
     public void register() {
 
             User user = new User(USERNAME, PASSWORD, "jack2@qq.com", RoleEnum.CUSTOMER.getCode());
             userService.register(user);
 
+    }
+
+    @Test
+    public void login() {
+        ResponseVo<User> responseVo = userService.login(USERNAME, PASSWORD);
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), responseVo.getStatus());
     }
 }
